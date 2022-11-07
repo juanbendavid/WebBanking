4fb35e0535e545a46817e92590e100724c9d7e52
Juan David <Juan David@DESKTOP-ICVFKPP>
2022-11-07 16:25:21 -0300

    colores

--- src/com/principal/Principal.java
+++ src/com/principal/Principal.java
@@ -19,7 +19,7 @@ public class Principal extends javax.swing.JFrame {
      */
     public Principal() {
         initComponents();
-        setImageLabel(jButton2, "src/com/images/aplicacion-wallet-pass.png");
+        setImageLabel(depositoBtn, "src/com/images/aplicacion-wallet-pass.png");
     }
 
     /**
@@ -40,12 +40,12 @@ public class Principal extends javax.swing.JFrame {
         separador = new javax.swing.JSeparator();
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
-        jButton5 = new javax.swing.JButton();
-        jButton6 = new javax.swing.JButton();
+        tarjetaBtn = new javax.swing.JButton();
+        depositoBtn = new javax.swing.JButton();
+        saldoBtn = new javax.swing.JButton();
+        transferenciasBtn = new javax.swing.JButton();
+        ingresarBtn = new javax.swing.JButton();
+        serviciosBtn = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -53,7 +53,7 @@ public class Principal extends javax.swing.JFrame {
         txtclave.setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
         txtclave.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jPanel1.setBackground(javax.swing.UIManager.getDefaults().getColor("Actions.Blue"));
+        jPanel1.setBackground(new java.awt.Color(0, 102, 102));
 
         jLabel3.setFont(new java.awt.Font("Roboto Medium", 0, 24)); // NOI18N
         jLabel3.setForeground(java.awt.SystemColor.control);
@@ -113,57 +113,62 @@ public class Principal extends javax.swing.JFrame {
         jLabel8.setText("Cuenta n° 46-7154546");
         txtclave.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 90, 250, -1));
 
-        jButton1.setBackground(javax.swing.UIManager.getDefaults().getColor("Actions.Blue"));
-        jButton1.setFont(new java.awt.Font("Roboto Medium", 0, 18)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText(" Pago de Tarjeta de Crédito");
-        jButton1.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        txtclave.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 390, 260, 40));
-
-        jButton2.setBackground(javax.swing.UIManager.getDefaults().getColor("Actions.Blue"));
-        jButton2.setFont(new java.awt.Font("Roboto Medium", 0, 18)); // NOI18N
-        jButton2.setForeground(new java.awt.Color(255, 255, 255));
-        jButton2.setText("Depósito");
-        jButton2.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        tarjetaBtn.setBackground(new java.awt.Color(0, 102, 102));
+        tarjetaBtn.setFont(new java.awt.Font("Roboto Medium", 0, 18)); // NOI18N
+        tarjetaBtn.setForeground(new java.awt.Color(255, 255, 255));
+        tarjetaBtn.setText(" Pago de Tarjeta de Crédito");
+        tarjetaBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        tarjetaBtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                tarjetaBtnActionPerformed(evt);
             }
         });
-        txtclave.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 330, 130, 40));
+        txtclave.add(tarjetaBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 390, 260, 40));
 
-        jButton3.setBackground(javax.swing.UIManager.getDefaults().getColor("Actions.Blue"));
-        jButton3.setFont(new java.awt.Font("Roboto Medium", 0, 18)); // NOI18N
-        jButton3.setForeground(new java.awt.Color(255, 255, 255));
-        jButton3.setText("Saldo");
-        jButton3.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        jButton3.addActionListener(new java.awt.event.ActionListener() {
+        depositoBtn.setBackground(new java.awt.Color(0, 102, 102));
+        depositoBtn.setFont(new java.awt.Font("Roboto Medium", 0, 18)); // NOI18N
+        depositoBtn.setForeground(new java.awt.Color(255, 255, 255));
+        depositoBtn.setText("Depósito");
+        depositoBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        depositoBtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+                depositoBtnActionPerformed(evt);
             }
         });
-        txtclave.add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 330, 150, 40));
+        txtclave.add(depositoBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 330, 130, 40));
 
-        jButton4.setBackground(javax.swing.UIManager.getDefaults().getColor("Actions.Blue"));
-        jButton4.setFont(new java.awt.Font("Roboto Medium", 0, 18)); // NOI18N
-        jButton4.setForeground(new java.awt.Color(255, 255, 255));
-        jButton4.setText("Transferencias");
-        jButton4.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        txtclave.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 330, 180, 40));
+        saldoBtn.setBackground(new java.awt.Color(0, 102, 102));
+        saldoBtn.setFont(new java.awt.Font("Roboto Medium", 0, 18)); // NOI18N
+        saldoBtn.setForeground(new java.awt.Color(255, 255, 255));
+        saldoBtn.setText("Saldo");
+        saldoBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        saldoBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                saldoBtnActionPerformed(evt);
+            }
+        });
+        txtclave.add(saldoBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 330, 150, 40));
 
-        jButton5.setBackground(javax.swing.UIManager.getDefaults().getColor("Actions.Blue"));
-        jButton5.setFont(new java.awt.Font("Roboto Medium", 0, 18)); // NOI18N
-        jButton5.setForeground(new java.awt.Color(255, 255, 255));
-        jButton5.setText("Ingresar");
-        jButton5.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        txtclave.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 330, 150, 40));
+        transferenciasBtn.setBackground(new java.awt.Color(0, 102, 102));
+        transferenciasBtn.setFont(new java.awt.Font("Roboto Medium", 0, 18)); // NOI18N
+        transferenciasBtn.setForeground(new java.awt.Color(255, 255, 255));
+        transferenciasBtn.setText("Transferencias");
+        transferenciasBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        txtclave.add(transferenciasBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 330, 180, 40));
 
-        jButton6.setBackground(javax.swing.UIManager.getDefaults().getColor("Actions.Blue"));
-        jButton6.setFont(new java.awt.Font("Roboto Medium", 0, 18)); // NOI18N
-        jButton6.setForeground(new java.awt.Color(255, 255, 255));
-        jButton6.setText("Pago de Servicios");
-        jButton6.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        txtclave.add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 390, 180, 40));
+        ingresarBtn.setBackground(new java.awt.Color(0, 102, 102));
+        ingresarBtn.setFont(new java.awt.Font("Roboto Medium", 0, 18)); // NOI18N
+        ingresarBtn.setForeground(new java.awt.Color(255, 255, 255));
+        ingresarBtn.setText("Ingresar");
+        ingresarBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        txtclave.add(ingresarBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(630, 330, 150, 40));
+
+        serviciosBtn.setBackground(new java.awt.Color(0, 102, 102));
+        serviciosBtn.setFont(new java.awt.Font("Roboto Medium", 0, 18)); // NOI18N
+        serviciosBtn.setForeground(new java.awt.Color(255, 255, 255));
+        serviciosBtn.setText("Pago de Servicios");
+        serviciosBtn.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        txtclave.add(serviciosBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 390, 180, 40));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -179,13 +184,17 @@ public class Principal extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+    private void saldoBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_saldoBtnActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton3ActionPerformed
+    }//GEN-LAST:event_saldoBtnActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void depositoBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_depositoBtnActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_depositoBtnActionPerformed
+
+    private void tarjetaBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tarjetaBtnActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_tarjetaBtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -233,19 +242,19 @@ public class Principal extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel contenedor;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JButton jButton6;
+    private javax.swing.JButton depositoBtn;
+    private javax.swing.JButton ingresarBtn;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JButton saldoBtn;
     private javax.swing.JSeparator separador;
+    private javax.swing.JButton serviciosBtn;
+    private javax.swing.JButton tarjetaBtn;
+    private javax.swing.JButton transferenciasBtn;
     private javax.swing.JPanel txtclave;
     // End of variables declaration//GEN-END:variables
 }
